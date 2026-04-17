b7ebeeb093eed630d7648e634fac54e7e6a66428
Xavier McClurkin <xavmcc3@gmail.com>
2026-04-16 23:30:42 -0400

    Added better favicons, updated gitignore, started webGL background

---
+++ app.js
@@ -0,0 +1,20 @@
+import * as THREE from 'three';
+
+const canvas = document.getElementById('bg');
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true
+});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
